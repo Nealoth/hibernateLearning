140e5a9f946363fb857e1d9f3151cd0b7a9542ba
Ilya <Ilya.Kornienko@twino.ru>
2020-07-12 20:58:40 +0300

    @FILTERDEF AND @FILTER CLASSIC

--- src/main/java/com/example/demo/model/Client.java
+++ src/main/java/com/example/demo/model/Client.java
@@ -2,14 +2,25 @@ package com.example.demo.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Filter;
+import org.hibernate.annotations.FilterDef;
+import org.hibernate.annotations.ParamDef;
+import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
 @Table(name = "clients")
 @Getter
 @Setter
+@FilterDef(
+		name = "doubleFilter",
+		parameters = @ParamDef(name = "doubleParam", type = "double"
+
+		)
+)
 public class Client {
 
 	@Id
@@ -19,10 +30,37 @@ public class Client {
 	@Column(name = "first_name")
 	private String firstName;
 
+	@Column
+	private Integer age;
+
+	@Column
+	private Double height;
+
 	@Column(name = "last_name")
 	private String lastName;
 
+	@Column(name = "deleted")
+	private Boolean deleted;
+
+	@Filter(
+			name = "doubleFilter",
+			condition = "balance = 0"
+	)
 	@OneToMany(mappedBy = "client")
-	private List<BankAccount> bankAccount;
+	private List<BankAccount> nullBankAccounts;
+
+	@OneToMany(mappedBy = "client")
+	@Filter(
+			name = "doubleFilter",
+			condition = "balance > :doubleParam"
+	)
+	private List<BankAccount> majorBankAccounts;
+
+	@Filter(
+			name = "doubleFilter",
+			condition = "balance < :doubleParam"
+	)
+	@OneToMany(mappedBy = "client")
+	private List<BankAccount> minorBankAccounts;
 
 }
