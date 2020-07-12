a41452559e867b4d5607840604964589bf18b307
Ilya <Ilya.Kornienko@twino.ru>
2020-07-12 20:58:21 +0300

    Revert "@WHERE"
    This reverts commit b33e8803

--- src/main/java/com/example/demo/model/Client.java
+++ src/main/java/com/example/demo/model/Client.java
@@ -2,20 +2,14 @@ package com.example.demo.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Filter;
-import org.hibernate.annotations.FilterDef;
-import org.hibernate.annotations.ParamDef;
-import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
 @Table(name = "clients")
 @Getter
 @Setter
-@Where(clause = "deleted = false")
 public class Client {
 
 	@Id
@@ -25,24 +19,10 @@ public class Client {
 	@Column(name = "first_name")
 	private String firstName;
 
-	@Column
-	private Integer age;
-
-	@Column
-	private Double height;
-
 	@Column(name = "last_name")
 	private String lastName;
 
-	@Column(name = "deleted")
-	private Boolean deleted;
-
 	@OneToMany(mappedBy = "client")
-	@Where(clause = "balance > 0.0")
-	private List<BankAccount> notNullBankAccounts;
-
-	@OneToMany(mappedBy = "client")
-	@Where(clause = "balance = 0.0")
-	private List<BankAccount> nullBankAccounts;
+	private List<BankAccount> bankAccount;
 
 }
