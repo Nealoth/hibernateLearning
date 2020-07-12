df26b1772cae7e6fc1b0cb0e228e7cb3e898d17f
Ilya <Ilya.Kornienko@twino.ru>
2020-07-12 21:00:59 +0300

    Revert "@FILTER ENABLE DISABLE"
    This reverts commit 9948ee48

--- src/main/java/com/example/demo/model/Client.java
+++ src/main/java/com/example/demo/model/Client.java
@@ -2,24 +2,14 @@ package com.example.demo.model;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.Filter;
-import org.hibernate.annotations.FilterDef;
-import org.hibernate.annotations.ParamDef;
-import org.hibernate.annotations.Where;
 
 import javax.persistence.*;
-import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
 @Table(name = "clients")
 @Getter
 @Setter
-@FilterDef(
-		name = "doubleFilter",
-		parameters = @ParamDef(name = "doubleParam", type = "double")
-)
-@Filter(name = "doubleFilter", condition = "height > :doubleParam")
 public class Client {
 
 	@Id
@@ -29,25 +19,10 @@ public class Client {
 	@Column(name = "first_name")
 	private String firstName;
 
-	@Column
-	private Integer age;
-
-	@Column
-	private Double height;
-
 	@Column(name = "last_name")
 	private String lastName;
 
-	@Column(name = "deleted")
-	private Boolean deleted;
-
 	@OneToMany(mappedBy = "client")
-	private List<BankAccount> nullBankAccounts;
-
-	@OneToMany(mappedBy = "client")
-	private List<BankAccount> majorBankAccounts;
-
-	@OneToMany(mappedBy = "client")
-	private List<BankAccount> minorBankAccounts;
+	private List<BankAccount> bankAccount;
 
 }
